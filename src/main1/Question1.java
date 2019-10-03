ae5f44c3ec2fea3273c643743f78fbe4c228a2c5
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2019-10-02 17:09:44 -0700

    print index of a input string

--- src/main1/Question1.java
+++ src/main1/Question1.java
@@ -6,8 +6,15 @@ public class Question1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
+        System.out.print("index :" + "\t");
+        for (int i = 0; i < input.length(); i++) {
+            if (i == 0) {
+                System.out.print(i + "\t" + "\t");
+            } else {
+                System.out.print(i + "\t");
+            }
+        }
 
-    }
 
     }
 }
