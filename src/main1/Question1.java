47c7c61cd819d35978840d45b36506da850ac30d
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2019-10-02 17:11:40 -0700

    final output :indexes with characters of a input string

--- src/main1/Question1.java
+++ src/main1/Question1.java
@@ -14,7 +14,15 @@ public class Question1 {
                 System.out.print(i + "\t");
             }
         }
-
+        System.out.println("");
+        System.out.print("chars :" + "\t");
+        for (int i = 0; i < input.length(); i++) {
+            if (i == 0) {
+                System.out.print(input.charAt(i) + "\t" + "\t");
+            } else {
+                System.out.print(input.charAt(i) + "\t");
+            }
+        }
 
     }
 }
