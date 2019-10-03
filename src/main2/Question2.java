ec274bdc3f6602c69728040dedf9a1f2442e7964
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2019-10-02 22:54:28 -0700

    take input from user

--- src/main2/Question2.java
+++ src/main2/Question2.java
@@ -2,8 +2,11 @@ package main2;
 
 import java.util.Scanner;
 
-public class Question2htthe {
+public class Question2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-    }
+        String input = sc.nextLine();
+        int f1 = sc.nextInt();
+        int f2 = sc.nextInt();
+
 }
