98147bd2a620fc19bf5e63ab5b5ea7ba533250c3
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2019-10-03 22:37:22 -0700

    convert string to upper case

--- src/main6/Question6.java
+++ src/main6/Question6.java
@@ -5,5 +5,7 @@ import java.util.Scanner;
 public class Question6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println(input.toUpperCase());
     }
 }
