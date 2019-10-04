ba6ea3fbfc1b130b5b363143e36a9423e38bed1b
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2019-10-03 23:27:17 -0700

    use method for uppercase

---
+++ src/main9/Question9.java
@@ -0,0 +1,21 @@
+package main9;
+
+import java.util.Scanner;
+
+public class Question9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        int uppersum = 0;
+        int lowersum = 0;
+        char c[] = input.toCharArray();
+        for (int i = 0; i < c.length; i++) {
+            if (Character.isUpperCase(c[i]))
+                uppersum += (int) c[i];
+
+        }
+
+    }
+}
+
+}
