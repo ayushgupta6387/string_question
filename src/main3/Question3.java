6215e91dc6f88418307f518d5f01f38cc7fde854
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2019-10-03 21:49:49 -0700

    print the output

--- src/main3/Question3.java
+++ src/main3/Question3.java
@@ -7,5 +7,12 @@ public class Question3 {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         String s = "aeiouAEIOU";
+        for (int i = 0; i < 10; i++) {
+            String r = s.charAt(i) + "";
+            input = input.replace(r, "*");
+        }
+        System.out.println(input);
+
+    }
 
 }
