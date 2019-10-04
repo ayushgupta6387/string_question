2859f796b057c8c57aa709498fa338d981789454
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2019-10-03 21:49:17 -0700

    store all vowels in 's'

--- src/main3/Question3.java
+++ src/main3/Question3.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Question3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String s = "aeiouAEIOU";
 
-    }
 }
