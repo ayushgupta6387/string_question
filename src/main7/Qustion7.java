a5a2a8a7f3db66c895c25d7afa09bb52f4279a9e
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2019-10-03 23:01:52 -0700

    add the ASCII values in a 'sum'

--- src/main7/Qustion7.java
+++ src/main7/Qustion7.java
@@ -5,5 +5,12 @@ import java.util.Scanner;
 public class Qustion7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        char c[] = input.toCharArray();
+        int sum = 0;
+        for (int i = 0; i < c.length; i++) {
+            sum = sum + (int) c[i];
+        }
+
     }
 }
