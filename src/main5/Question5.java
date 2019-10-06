5f5b871fca1ba8f193735f7201023d12bbee0a49
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2019-10-05 22:33:33 -0700

    take input from user

--- src/main5/Question5.java
+++ src/main5/Question5.java
@@ -1,7 +1,12 @@
 package main5;
 
+import java.util.Scanner;
+
 public class Question5 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
 
     }
 }
