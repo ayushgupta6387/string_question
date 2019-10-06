cab7d448e1a06644a6bcb40a50c560b1868a15df
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2019-10-05 22:07:40 -0700

    create main method

--- src/main10/Question10.java
+++ src/main10/Question10.java
@@ -5,7 +5,14 @@ import java.util.Scanner;
 public class Question10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        char ch = sc.next().charAt(0);
+        char cs[] = input.toCharArray();
+        for (int i = 0; i < input.length(); i++) {
+            if (cs[i] == ch) {
+                System.out.println(i);
 
-
+            }
+        }
     }
 }
