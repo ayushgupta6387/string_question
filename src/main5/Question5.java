541b29b524a5250f0136deec49a71e4b5611a6b2
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2019-10-05 22:36:34 -0700

    take input from user which substring want to replace

--- src/main5/Question5.java
+++ src/main5/Question5.java
@@ -6,6 +6,8 @@ public class Question5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
+        System.out.println("Enter substring you want to replace");
+        String input1 = sc.nextLine();
 
 
     }
